a5440507a17bf32048d299f5e1b83761e1d0ae63
Andrés López <andflopezbec@unal.edu.co>
2019-06-14 23:41:40 -0500

    Fix: change in url endpoint countTrips

--- backend_test/backend_test/trips/urls.py
+++ backend_test/backend_test/trips/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from backend_test.trips.views import countTrips, TripView
 
 apiTripsPatterns = ([
-    path('count-trips/', countTrips , name='count_trips'),
     path('trips/', TripView.as_view() , name='trips-list'),
+    path('trips/count', countTrips , name='count_trips'),
 
 ], 'api_trips')
