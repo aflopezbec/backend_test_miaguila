64713446f8a034012451839be25c1784ea417c00
Andrés López <andflopezbec@unal.edu.co>
2019-06-14 23:34:33 -0500

    Feat: update trips url

--- backend_test/backend_test/trips/urls.py
+++ backend_test/backend_test/trips/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from backend_test.trips.views import countTrips
+from backend_test.trips.views import countTrips, TripView
 
 apiTripsPatterns = ([
     path('count-trips/', countTrips , name='count_trips'),
+    path('trips/', TripView.as_view() , name='trips-list'),
 
 ], 'api_trips')
